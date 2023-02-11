b1265640527d6f612ae8798189601d9cd95578c2
Antonio Fulgêncio <antoniofulg@gmail.com>
2023-02-11 01:20:06 -0300

    feat: ensure Navbar render correctly navigation

---
+++ src/presentation/components/Navbar/Navbar.spec.tsx
@@ -0,0 +1,43 @@
+import { act, render, screen } from "@testing-library/react";
+import Navbar from "./Navbar";
+
+const makeSut = () => {
+  const pages = [
+    {
+      path: "",
+      label: "Home",
+    },
+    {
+      path: "",
+      label: "About",
+    },
+  ];
+
+  render(<Navbar pages={pages} />);
+};
+
+describe("<Navbar />", () => {
+  it("Should render navbar with navigation to correct pages", () => {
+    makeSut();
+
+    expect(screen.getByRole("link", { name: "Home" })).toBeInTheDocument();
+  });
+
+  it("Should render navbar with default navigation and navigation passed as props", () => {
+    makeSut();
+
+    expect(screen.getAllByRole("link")).toHaveLength(3);
+  });
+
+  it("Should hide navigation as default and show after menu button click on small screens", () => {
+    makeSut();
+
+    expect(screen.getByRole("navigation-list")).toHaveClass("opacity-0");
+
+    act(() => {
+      screen.getByRole("button").click();
+    });
+
+    expect(screen.getByRole("navigation-list")).toHaveClass("opacity-100");
+  });
+});
---
+++ src/presentation/components/Navbar/Navbar.tsx
@@ -0,0 +1,59 @@
+import { useState } from "react";
+import Icons from "../Icons/Icons";
+
+type NavbarProps = {
+  pages: Page[];
+};
+
+type Page = {
+  label: string;
+  path: string;
+};
+
+const Navbar = ({ pages }: NavbarProps) => {
+  const [open, setOpen] = useState(false);
+
+  return (
+    <nav className="bg-white border-gray-200 px-2 sm:px-4 py-2.5 dark:bg-gray-900">
+      <div className="container flex flex-wrap items-center justify-between mx-auto">
+        <a href="#" className="flex items-center">
+          <span className="self-center text-xl font-semibold whitespace-nowrap dark:text-white">
+            Tira Times
+          </span>
+        </a>
+        <button
+          data-collapse-toggle="navbar-default"
+          type="button"
+          className="inline-flex items-center p-2 ml-3 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+          aria-controls="navbar-default"
+          aria-expanded="false"
+          onClick={() => setOpen((prev) => !prev)}
+        >
+          <Icons type={open ? "x" : "bars"} />
+        </button>
+        <div className="w-full md:block md:w-auto" id="navbar-default">
+          <ul
+            className={`md:static md:flex flex-col p-4 mt-4 border border-gray-100 bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700 md:z-auto z-[-1] left-0 w-full top-10 absolute transition-all duration-500 ease-in ${
+              open ? "top-10 opacity-100" : "top-[-400px]"
+            } md:opacity-100 opacity-0`}
+            role="navigation-list"
+          >
+            {pages.map((page) => (
+              <li key={page.label}>
+                <a
+                  href="#"
+                  className="block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
+                  aria-current="page"
+                >
+                  {page.label}
+                </a>
+              </li>
+            ))}
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
