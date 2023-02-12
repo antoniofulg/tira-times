4a6b4c2122f0f32efaea14e95029367ba130b163
Antonio Fulgêncio <antoniofulg@gmail.com>
2023-02-12 15:11:34 -0300

    fix: change card to fill all height space

--- src/presentation/components/Card/Card.tsx
+++ src/presentation/components/Card/Card.tsx
@@ -12,7 +12,7 @@ const Card = ({ title, description, link }: CardProps) => {
       <Link
         to={link}
         role="link"
-        className="block p-6 bg-white border border-gray-200 rounded-lg shadow-md hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700"
+        className="block h-full p-6 bg-white border border-gray-200 rounded-lg shadow-md hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700"
       >
         <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
           {title}
