dbf3cd2a3d2f0fda0550df7ae35b9c867ab53234
Antonio Fulgêncio <antoniofulg@gmail.com>
2023-02-12 12:48:36 -0300

    feat: add team icon

--- src/presentation/components/Navbar/Navbar.tsx
+++ src/presentation/components/Navbar/Navbar.tsx
@@ -18,7 +18,8 @@ const Navbar = ({ pages }: NavbarProps) => {
     <nav className="bg-white border-gray-200 px-2 sm:px-4 py-2.5 dark:bg-gray-900">
       <div className="container flex flex-wrap items-center justify-between mx-auto">
         <Link to="/" className="flex items-center">
-          <span className="self-center text-xl font-semibold whitespace-nowrap dark:text-white">
+          <Icons type="team" />
+          <span className="self-center ml-2 text-xl font-semibold whitespace-nowrap dark:text-white">
             Tira Times
           </span>
         </Link>
