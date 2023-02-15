22c9404cba6dbe893f849b9d6ad56d33400984b8
Antonio Fulgêncio <antoniofulg@gmail.com>
2023-02-14 21:43:44 -0300

    feat: add objectTypeChecker function

--- src/utils/types/forms.spec.ts
+++ src/utils/types/forms.spec.ts
@@ -1,20 +1,59 @@
 import { describe, it } from "vitest";
-import { typeChecker } from "./forms";
+import { objectTypeChecker, typeChecker } from "./forms";
 
 describe("utils/types/forms.ts -> typeChecker", () => {
-  it("Should return true when a field exists in the data provided", () => {
-    type Data = { field: boolean };
+  type Data = { field: boolean };
 
-    const data: Data = { field: true };
+  const makeSut = (): Data => ({ field: true });
+
+  it("Should return true when a field exists in the provided data", () => {
+    const data = makeSut();
 
     expect(typeChecker<Data>(data, "field")).toBe(true);
   });
 
-  it("Should return false when a field does not exists in the data provided", () => {
-    type Data = { field: boolean };
-
-    const data: Data = { field: true };
+  it("Should return false when a field does not exists in the provided data", () => {
+    const data = makeSut();
 
     expect(typeChecker<Data>(data, "invalid")).toBe(false);
+  });
+});
+
+describe("utils/types/forms.ts -> objectTypeChecker", () => {
+  type Data = { field: boolean; field2: string; field3: number };
+
+  const makeSut = (
+    data: { [keys: string]: unknown } = {
+      field: false,
+      field2: "testing",
+      field3: 20,
+    }
+  ) => {
+    const object: Data = { field: true, field2: "", field3: 0 };
+
+    return { object, data };
+  };
+
+  it("Should return true when a all field of object exists in the provided data", () => {
+    const { data, object } = makeSut();
+
+    expect(objectTypeChecker(data, object)).toBe(true);
+  });
+
+  it("Should return false when some field of object does not exists in the provided data", () => {
+    const { data, object } = makeSut({ field: true, field2: "" });
+
+    expect(objectTypeChecker(data, object)).toBe(false);
+  });
+
+  it("Should return true even if some field in provided data does not exists in the object", () => {
+    const { data, object } = makeSut({
+      field: true,
+      field2: "",
+      field3: 0,
+      field4: "",
+    });
+
+    expect(objectTypeChecker(data, object)).toBe(true);
   });
 });
--- src/utils/types/forms.ts
+++ src/utils/types/forms.ts
@@ -1,6 +1,16 @@
-export const typeChecker = <T extends { [keys: string]: unknown }>(
-  data: { [keys: string]: unknown },
+type ObjectExtender = { [keys: string]: unknown };
+
+export const typeChecker = <T extends ObjectExtender>(
+  data: ObjectExtender,
   field: string
 ): data is T => {
   return (data as T)[field] !== undefined;
+};
+
+export const objectTypeChecker = <T extends ObjectExtender>(
+  data: ObjectExtender,
+  object: T
+): data is T => {
+  const keys = Object.keys(object);
+  return keys.every((key) => typeChecker<T>(data, key));
 };
