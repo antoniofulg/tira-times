3f098aa5bfc6eba3570574c942b539f816a158b9
Antonio Fulgêncio <antoniofulg@gmail.com>
2023-02-15 17:07:27 -0300

    test: add tests for different props types

--- src/presentation/components/Button/Button.spec.tsx
+++ src/presentation/components/Button/Button.spec.tsx
@@ -3,8 +3,50 @@ import Button from "./Button";
 
 describe("<Button />", () => {
   it('Should have `role="button"` by default', () => {
-    render(<Button color={"primary"} />);
+    render(<Button />);
 
     expect(screen.getByRole("button")).toBeInTheDocument();
+  });
+
+  it("Should render a different role when passed as param", () => {
+    render(<Button role="submit" />);
+
+    expect(screen.getByRole("submit")).toBeInTheDocument();
+  });
+
+  it("Should render a disabled status button", () => {
+    render(<Button disabled />);
+
+    expect(screen.getByRole("button")).toHaveAttribute("disabled");
+    expect(screen.getByRole("button")).toHaveClass("disabled");
+  });
+
+  it("Should render loading status button", () => {
+    render(<Button loading />);
+
+    expect(screen.getByRole("button")).toHaveAttribute("disabled");
+    expect(screen.getByRole("button")).toHaveClass("loading");
+  });
+
+  it("Should render correct class when primary color is provided", () => {
+    render(<Button color="primary" />);
+
+    expect(screen.getByRole("button")).toHaveClass("primary");
+  });
+
+  it("Should render correct class when secondary color is provided", () => {
+    render(<Button color="secondary" />);
+
+    expect(screen.getByRole("button")).toHaveClass("secondary");
+  });
+
+  it("Should render no class when color is not provided", () => {
+    render(<Button />);
+
+    const button = screen.getByRole("button");
+
+    expect(button).toHaveClass("unstyled");
+    expect(button).not.toHaveClass("primary");
+    expect(button).not.toHaveClass("secondary");
   });
 });
