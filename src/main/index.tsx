900e10a52bf401d6b03e2eb0f7e8e108d385644f
Antonio Fulgêncio <antoniofulg@gmail.com>
2023-02-15 17:27:25 -0300

    refactor: move css style sheet to presentation layer

--- src/main/index.tsx
+++ src/main/index.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
-import "./index.css";
+import "@/presentation/global.css";
 import router from "./routes/router";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
