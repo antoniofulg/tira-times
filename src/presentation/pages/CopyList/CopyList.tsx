fe5daf76907541fe799e053b42e76fb825721c02
Antonio Fulgêncio <antoniofulg@gmail.com>
2023-02-19 19:06:52 -0300

    feat: create copy-list page

---
+++ src/presentation/pages/CopyList/CopyList.tsx
@@ -0,0 +1,11 @@
+const CopyList = () => {
+  return (
+    <div className="container px-4 pt-8 mx-auto">
+      <h1 className="mb-4 text-4xl font-extrabold leading-none tracking-tight text-center text-gray-900 md:text-5xl lg:text-6xl dark:text-white">
+        Selecione o estilo de lista que você queira criar!
+      </h1>
+    </div>
+  );
+};
+
+export default CopyList;
