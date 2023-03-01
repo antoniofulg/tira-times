e06da18dbd02a5f4624e7aff5611bf805dd79b0c
Antonio Fulgêncio <antoniofulg@gmail.com>
2023-03-01 01:21:41 -0300

    refactor: move layouts and routes to app folder

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,8 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
-import "./global.css";
-import router from "@/routes/router";
+import "@/global.css";
+import router from "./routes/router";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
--- src/app/routes/router.tsx
+++ src/app/routes/router.tsx
@@ -1,11 +1,11 @@
-import Main from "@/layouts/Main";
+import { MainLayout } from "@/app/components/layouts";
 import { MatchInfo, Home, CopyList } from "@/pages";
 import { createBrowserRouter } from "react-router-dom";
 
 export default createBrowserRouter([
   {
     path: "/",
-    element: <Main />,
+    element: <MainLayout />,
     children: [
       {
         path: "",
