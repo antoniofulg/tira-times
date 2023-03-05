24f2e762e16af3fc8d0c82bcd814c696c2705e0f
Antonio Fulgêncio <antoniofulg@gmail.com>
2023-03-05 18:46:32 -0300

    refactor: update router structure to JSX style

--- src/app/routes/router.tsx
+++ src/app/routes/router.tsx
@@ -1,30 +1,18 @@
 import { MainLayout } from "@/app/components/layouts";
 import { MatchInfo, Home, ChooseListStyle, CopyList } from "@/pages";
-import { createBrowserRouter } from "react-router-dom";
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+} from "react-router-dom";
 
-export default createBrowserRouter([
-  {
-    path: "/",
-    element: <MainLayout />,
-    children: [
-      {
-        path: "",
-        element: <Home />,
-      },
-      {
-        path: "match-info",
-        element: <MatchInfo />,
-      },
-      {
-        path: "copy-list",
-        element: <ChooseListStyle />,
-        children: [
-          {
-            path: ":style",
-            element: <CopyList />,
-          },
-        ],
-      },
-    ],
-  },
-]);
+export default createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<MainLayout />}>
+      <Route index element={<Home />} />
+      <Route path="match-info" element={<MatchInfo />} />
+      <Route path="copy-list" element={<ChooseListStyle />} />
+      <Route path="copy-list/:style" element={<CopyList />} />
+    </Route>
+  )
+);
