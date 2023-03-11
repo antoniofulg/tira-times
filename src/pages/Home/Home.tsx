c7622be6c45e64c8946ca59444d0a59fcc5204c9
Antonio Fulgêncio <antoniofulg@gmail.com>
2023-03-11 16:48:15 -0300

    feat: improve texts of home page

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -4,27 +4,30 @@ const Home = () => {
   return (
     <div className="container px-4 pt-8 mx-auto">
       <h1 className="mb-4 text-4xl font-extrabold leading-none tracking-tight text-center text-gray-900 md:text-5xl lg:text-6xl dark:text-white">
-        O jeito mais fácil de sortear os times do seu racha
+        Sorteie os times do seu racha de forma rápida e fácil com o nosso
+        aplicativo inteligente!
       </h1>
       <p className="mb-6 text-lg font-normal text-center text-gray-500 lg:text-xl sm:px-16 xl:px-48 dark:text-gray-400">
-        Gere uma lista de jogadores com todas as informações do seu racha, e
-        então sorteie os times com várias opções de balanceamento para deixar as
-        partidas mais equilibradas!
+        Com o nosso aplicativo, você pode facilmente criar uma lista de
+        jogadores para o seu racha e deixar que a nossa tecnologia inteligente
+        faça o trabalho pesado para você. Usando algoritmos avançados, o
+        aplicativo irá automaticamente sortear os times de forma balanceada para
+        que cada partida seja justa e equilibrada.
       </p>
 
       <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-6">
         <div className="lg:col-span-2 lg:col-start-2">
           <Card
-            title="Criar lista"
-            description="Insira as principais informações do seu racha para criar uma lista personalizada pronta para enviar para seu grupo."
+            title="Desejo criar uma lista"
+            description="Crie uma lista personalizada para o seu racha em segundos! Insira as principais informações do seu evento, e deixe que nosso aplicativo faça o resto. Em poucos cliques, você terá uma lista pronta para enviar para o seu grupo e começar a planejar a sua partida."
             link="/match-info"
           />
         </div>
         <div className="lg:col-span-2">
           <Card
-            title="Sortear times"
-            description="Insira uma lista gerada anteriormente já preenchida para buscar o nome dos jogadores automaticamente"
-            link="/draft"
+            title="Já possuo uma lista preenchida"
+            description="Não perca tempo digitando nomes novamente! Importe facilmente uma lista de jogadores preenchida anteriormente para o nosso aplicativo e deixe que ele busque automaticamente os nomes dos jogadores para você."
+            link="/paste-list"
           />
         </div>
       </div>
