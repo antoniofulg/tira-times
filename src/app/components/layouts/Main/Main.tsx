b743fda62578d7c03a84c850935a474ddb17427b
Antonio Fulgêncio <antoniofulg@gmail.com>
2023-03-11 15:30:22 -0300

    chore: add react-toastify library

--- src/app/components/layouts/Main/Main.tsx
+++ src/app/components/layouts/Main/Main.tsx
@@ -1,9 +1,12 @@
 import { Header } from "@/components";
 import { Outlet } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 const Main = () => {
   return (
     <>
+      <ToastContainer theme="dark" position="bottom-right" />
       <Header />
       <main className="h-full min-h-screen bg-white dark:bg-gray-900">
         <Outlet />
