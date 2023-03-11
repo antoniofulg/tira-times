7b1dab2be0f65991729f22e5b46fe6a0b9d068a6
Antonio Fulgêncio <antoniofulg@gmail.com>
2023-03-11 15:31:57 -0300

    refactor: change native alerts for react toastify

--- src/pages/ChooseListStyle/ChooseListStyle.tsx
+++ src/pages/ChooseListStyle/ChooseListStyle.tsx
@@ -1,15 +1,22 @@
 import { getMatchInfo } from "./features/get-match-info";
 import ChooseListStyleForm from "./components/ChooseListStyleForm/ChooseListStyleForm";
 import NoListFound from "./components/NoListFound/NoListFound";
-import { parseMatchInfo } from "./schemas/match-info-schemas";
-
-const matchInfoInput = getMatchInfo();
-const matchInfo = matchInfoInput ? parseMatchInfo(matchInfoInput) : null;
+import { parseMatchInfo, MatchInfo } from "./schemas/match-info-schemas";
+import { toast } from "react-toastify";
+import { useEffect, useState } from "react";
 
 const ChooseListStyle = () => {
+  const [matchInfo, setMatchInfoInput] = useState<MatchInfo | null>(null);
   const submitHandler = (list: string) => {
+    toast.success("Lista copiada, agora é só compartilhar com sua turma!");
     navigator.clipboard.writeText(list);
   };
+
+  useEffect(() => {
+    const matchInfoInput = getMatchInfo();
+    const matchInfo = matchInfoInput ? parseMatchInfo(matchInfoInput) : null;
+    setMatchInfoInput(matchInfo);
+  }, []);
 
   return (
     <div className="container px-4 pt-8 mx-auto">
